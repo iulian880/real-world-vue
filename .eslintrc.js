a2afe1fb026ed0c6fa20b1642735c2bfcb308cdf
Iulian Petcu <iulian.petcu98@e-uvt.ro>
2020-04-06 01:24:47 +0300

    code editor settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,18 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: ["plugin:vue/essential", 
+  "eslint:recommended", 
+  "plugin:prettier/recommanded",
+  "@vue/prettier"],
   parserOptions: {
     parser: "babel-eslint"
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
-  }
+  },
+parserOptions: {
+  parser: "babel-eslint"
+}
 };
