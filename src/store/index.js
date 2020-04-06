f6c3b180f9fdc366f0821399e754ba0af30d8858
Iulian Petcu <iulian.petcu98@e-uvt.ro>
2020-04-06 19:26:24 +0300

    vuex

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,18 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    user: { id: 'abc123', name: 'Petcu Iulian' },
+    categories: [
+      'sutstainability',
+      'nature',
+      'animal welfare',
+      'housing',
+      'education',
+      'food',
+      'community'
+    ]
+  },
   mutations: {},
   actions: {},
   modules: {}
