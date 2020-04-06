2c98cca7fd2f7f4ea6ae49476d64ca26d6037c69
Iulian Petcu <iulian.petcu98@e-uvt.ro>
2020-04-06 18:23:24 +0300

    api calls with axios

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -7,9 +7,22 @@
 
 <script>
 import EventCard from '@/components/EventCard.vue'
+import axios from 'axios'
+
 export default {
   components: {
     EventCard // Shorthand for ES6 EventCard: EventCard
+  },
+
+  created() {
+    axios
+      .get('http://localhost:3000/events') // Does a get request
+      .then(response => {
+        console.log(response.data) // For now, logs out the response
+      })
+      .catch(error => {
+        console.log('There was an error:', error.response) // Logs out the error
+      })
   }
 }
 </script>
