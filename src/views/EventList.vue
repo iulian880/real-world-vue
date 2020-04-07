853378638bbef4685d12d728d7c1808074f2aab3
Iulian Petcu <iulian.petcu98@e-uvt.ro>
2020-04-07 15:08:12 +0300

    creating events ADD_EVENT Mutation The createEvent Action Dispatching the eventCreate Action Resetting our Event Data Routing to our New Event

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -4,7 +4,6 @@
     <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
 </template>
-
 <script>
 import EventCard from '@/components/EventCard.vue'
 import EventService from '@/services/EventService.js'
